17b02c5297a9147ca3a3c0a8867903b67e05e08c
kabuqinuofu <15237853051@163.com>
2019-11-29 08:49:49 +0800

    Test

---
+++ 中国知网使用协议 & 中国知网隐私政策_files/screen.js
@@ -0,0 +1,18 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=425){
+                docEl.style.fontSize = '55px';
+            }else{
+                docEl.style.fontSize = 85 * (clientWidth / 640) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
